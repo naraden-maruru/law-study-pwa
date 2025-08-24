672235ffe77502344cf1517a01deead25876c67c
naraden-maruru <kukki.go@gmail.com>
2025-08-25 08:39:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.clients.claim());
+
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { self.clients.claim(); });
+self.addEventListener('fetch', e => {
+  // network-first with fallback
+  e.respondWith((async () => {
+    try { return await fetch(e.request); } catch (err) { return caches.match(e.request) || Response.error(); }
+  })());
+});
